a365e7479e4b85203aca2a5487e38c17c35a867b
junseok-dev <eric676392@gmail.com>
2026-03-13 15:45:00 +0900

    feat: 배포는 성공이고 홈페이지야 나와라

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from chat.views import chat_index
 
 urlpatterns = [
+    path('', chat_index, name='home'),
     path('admin/', admin.site.urls),
     path('api/auth/', include('dj_rest_auth.urls')),
     path('api/auth/registration/', include('dj_rest_auth.registration.urls')),
